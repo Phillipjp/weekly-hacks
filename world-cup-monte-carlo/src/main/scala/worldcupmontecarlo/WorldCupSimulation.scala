3482f40e123f006464f97e279d814b25c798c74f
Phillip Perks <p.perks@elsevier.com>
2019-06-26 15:34:05 +0100

    put odds in it's own file

--- world-cup-monte-carlo/src/main/scala/worldcupmontecarlo/WorldCupSimulation.scala
+++ world-cup-monte-carlo/src/main/scala/worldcupmontecarlo/WorldCupSimulation.scala
@@ -55,35 +55,6 @@ class WorldCupSimulation {
   }
 }
 
-case class Odds(numerator: Int, denominator: Int) {
-
-  private def calculateGCD(a: Int, b: Int): Int = {
-    if(a == 0)
-      b
-    else if(b == 0)
-      a
-    else
-      calculateGCD(b, a % b)
-  }
-
-  private def normalise(): String = {
-
-    val gcd = calculateGCD(numerator , denominator)
-    val normalisedNumerator = numerator/gcd
-    val normalisedDenominator = denominator/gcd
-
-    normalisedNumerator + "/" + normalisedDenominator
-  }
-
-  def decimal(): Double = {
-    numerator.toDouble/denominator.toDouble
-  }
-
-  override def toString: String = {
-    normalise()
-  }
-}
-
 case class Team(name: String, rating: Int)
 
 object WorldCupSimulation {
