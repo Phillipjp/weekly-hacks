b75b05b6fb61884a276589b06a9287705044b767
Phillip Perks <p.perks@elsevier.com>
2019-08-29 10:56:50 +0100

    poker - make n of a kind hand

--- poker/src/main/scala/com/HandMaker.scala
+++ poker/src/main/scala/com/HandMaker.scala
@@ -10,7 +10,7 @@ object HandMaker {
 
     val flushSuit = cards.groupBy(c => c.suit).mapValues(_.size).filter{case(_, size) => size > 4}.head._1
     val scoringCards = cards.filter(card => card.suit == flushSuit).sortBy(-_.value.id).take(5)
-    val kickers = cards.filter(card => !scoringCards.contains(card)).sortBy(-_.value.id)
+    val kickers = getKickers(cards, scoringCards)
     val royal = Seq(Ace, King, Queen, Jack, Ten)
     if(scoringCards.map(card => card.value) == royal)
       Hand(scoringCards, kickers, royalFlushRank)
@@ -18,4 +18,25 @@ object HandMaker {
       Hand(scoringCards, kickers, flushRank)
   }
 
+  def makeNOfAKind(cards: Seq[Card], n: Int): Hand ={
+    val scoringCards: Seq[Card] = scoringCardsForOfAKind(cards, n)
+    val kickers = getKickers(cards, scoringCards)
+    n match {
+      case 4 => Hand(scoringCards, kickers, fourOfAKindRank)
+      case 3 => Hand(scoringCards, kickers, threeOfAKindRank)
+      case 2 => Hand(scoringCards, kickers, pairRank)
+    }
+  }
+
+  private def getKickers(cards: Seq[Card], scoringCards: Seq[Card]) =
+    cards.filter(card => !scoringCards.contains(card)).sortBy(-_.value.id)
+
+
+  private def scoringCardsForOfAKind(cards: Seq[Card], n: Int) = {
+    cards
+      .groupBy(c => c.value.id)
+      .filter { case (_, groupedCards) => groupedCards.size == n }
+      .toSeq.maxBy { case (cardValue, _) => cardValue }
+      ._2
+  }
 }
--- poker/src/test/scala/com/HandMakerSpec.scala
+++ poker/src/test/scala/com/HandMakerSpec.scala
@@ -22,6 +22,21 @@ class HandMakerSpec extends FlatSpec with Matchers {
 
     makeFlush(cards2) should equal(Hand(Seq(Card(SPADE, Ace),Card(SPADE, King),Card(SPADE, Queen),Card(SPADE, Jack),Card(SPADE, Ten)),
       Seq(Card(SPADE, Eight),Card(SPADE, Seven)), royalFlushRank))
+  }
+
+  it should "make an N of a kind hand" in {
+    val cards1 = Seq(Card(SPADE, Two), Card(DIAMOND, Two), Card(CLUB, Two), Card(HEART, Two), Card(SPADE, Three), Card(DIAMOND, Three))
+
+    val cards2 = Seq(Card(SPADE, Two), Card(DIAMOND, Two), Card(CLUB, Two), Card(HEART, Four), Card(SPADE, Three), Card(DIAMOND, Three))
+
+    val cards3 = Seq(Card(SPADE, Two), Card(DIAMOND, Two), Card(CLUB, Three), Card(HEART, Four), Card(SPADE, Five), Card(DIAMOND, Six))
+
+    val cards4 = Seq(Card(SPADE, Two), Card(DIAMOND, Two), Card(CLUB, Three), Card(HEART, Three), Card(SPADE, Five), Card(DIAMOND, Six))
+
+    makeNOfAKind(cards1, 4) should equal(Hand(Seq(Card(SPADE, Two), Card(DIAMOND, Two), Card(CLUB, Two), Card(HEART, Two)), Seq(Card(SPADE, Three), Card(DIAMOND, Three)), fourOfAKindRank))
+    makeNOfAKind(cards2, 3) should equal(Hand(Seq(Card(SPADE, Two), Card(DIAMOND, Two), Card(CLUB, Two)), Seq(Card(HEART, Four), Card(SPADE, Three), Card(DIAMOND, Three)), threeOfAKindRank))
+    makeNOfAKind(cards3, 2) should equal(Hand(Seq(Card(SPADE, Two), Card(DIAMOND, Two)), Seq(Card(DIAMOND, Six), Card(SPADE, Five), Card(HEART, Four), Card(CLUB, Three)), pairRank))
+    makeNOfAKind(cards4, 2) should equal(Hand(Seq(Card(CLUB, Three), Card(HEART, Three)), Seq(Card(DIAMOND, Six), Card(SPADE, Five),Card(SPADE, Two), Card(DIAMOND, Two)), pairRank))
 
   }
 }
