f1ed89b9fd27a4a1f26e34074487fea86952044d
phillipjp <phillipjperks@gmail.com>
2019-10-12 16:15:42 +0100

    [word-funnel] bonus 2 (incomplete)

--- word-funnel/src/main/scala/wordfunnel/Main.scala
+++ word-funnel/src/main/scala/wordfunnel/Main.scala
@@ -6,11 +6,18 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val dict = DictionaryReader.readInDictionary("/Users/phillipperks/Documents/repos/weekly-hacks/word-funnel/src/main/resources/enable1.txt")
-    val maxFunnelLength = funnel(Seq("gnash"), dict, getFunneledWords)
-    println(maxFunnelLength)
+//    val maxFunnelLength = funnel(Seq("gnash"), dict, getFunneledWords)
+//    println(maxFunnelLength)
+//
+    /*Bonus 1*/
+//    val biggestFunnel = dict.filter(word => word.length > 10).map(word => (word, WordFunnel.funnel(Seq(word), dict, getFunneledWords))).maxBy{ case(_, funnelLength) => funnelLength}
+//    println(biggestFunnel)
+    ////(complecting,10)
 
-    val biggestFunnel = dict.filter(word => word.length > 10).map(word => (word, WordFunnel.funnel(Seq(word), dict, getFunneledWords))).maxBy{ case(_, funnelLength) => funnelLength}
-    println(biggestFunnel)
-    //(complecting,10)
+    /*Bonus 2*/
+    val biggestFunnel2 = dict.filter(word => word.length > 10).map(word => (word, WordFunnel.funnel(Seq(word), dict, getFunneledWords2))).toSeq.sortBy{ case(_, funnelLength) => - funnelLength}.take(7)
+    biggestFunnel2.foreach(println)
+    //(contradictorinesses,12)
+    //(preformationists,12)
   }
 }
