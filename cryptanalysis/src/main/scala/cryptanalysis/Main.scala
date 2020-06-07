90cbbeb2f567befea99db0a4ee55c39419c4c721
Phillip Perks <p.perks@elsevier.com>
2020-06-07 21:42:13 +0100

    [Crtyptanalysis] clean up main

--- cryptanalysis/src/main/scala/cryptanalysis/Main.scala
+++ cryptanalysis/src/main/scala/cryptanalysis/Main.scala
@@ -1,6 +1,7 @@
 package cryptanalysis
 
 import scala.io.{BufferedSource, Source}
+import scala.util.Random
 
 object Main {
 
@@ -9,9 +10,8 @@ object Main {
     val cypher = new CaesarCypherEncrypter(2)
     val plainText = "the quick brown fox jumps over the lazy dog"
     val cypherText = cypher.encrypt(plainText)
-    val source  = Source.fromResource("bee-movie-script.txt")
-    val sampleText = source.mkString
-    source.close()
+
+    val sampleText = "louofumnhtwroxrkzqojthaepdyecbvsegi"
 
     val decrypter = new CaesarCypherDecrypter(sampleText)
     val result = decrypter.decryptCypherText(cypherText)
