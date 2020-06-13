28cec3538a5f995cd5880fd2ba9c9eec007f62f1
Phillip Perks <p.perks@elsevier.com>
2020-06-13 11:06:59 +0100

    [cryptanalysis] parse in letter frequencies

--- cryptanalysis/src/main/scala/cryptanalysis/caesar/CaesarBreaker.scala
+++ cryptanalysis/src/main/scala/cryptanalysis/caesar/CaesarBreaker.scala
@@ -7,22 +7,18 @@ import scala.annotation.tailrec
 
 object CaesarBreaker extends Breaker[Int]{
 
-  // Sourced from: http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
-  private val letterProbabilities = Seq(8.12, 1.49, 2.71, 4.32, 12.02, 2.3, 2.03, 5.92, 7.31, 0.1, 0.69, 3.98, 2.61,
-    6.95, 7.68, 1.82, 0.11, 6.02, 6.28, 9.1, 2.88, 1.11, 2.09, 0.17, 2.11, 0.07)
-
 
   override def break(cipherText: String, language: Language): Seq[(Int, String)] = {
     val normalizedCipherText = language.normalizeStringWithoutSpaces(cipherText)
-    val expectedLetterFrequencies = calculateExpectedLetterFrequencies(normalizedCipherText)
+    val expectedLetterFrequencies = calculateExpectedLetterFrequencies(normalizedCipherText, language.letterProbabilities)
 
     val initialFrequencies = language.alphabet.map(letter => (letter, 1)).toMap
 
-    val shiftChiSquareScores = (0 until language.alphabet.size).map(shift => getChiSquaredScoreForShift(cipherText, expectedLetterFrequencies, initialFrequencies, shift, language))
+    val shiftChiSquareScores = (0 until language.alphabet.length).map(shift => getChiSquaredScoreForShift(cipherText, expectedLetterFrequencies, initialFrequencies, shift, language))
 
     val closestShifts = shiftChiSquareScores.zipWithIndex.sortBy(_._1).take(5).map(_._2)
 
-    closestShifts.map{shift => (language.alphabet.size - shift, CaesarCipher.encrypt(cipherText, shift, language))}
+    closestShifts.map{shift => (language.alphabet.length - shift, CaesarCipher.encrypt(cipherText, shift, language))}
   }
 
   private def getChiSquaredScoreForShift(cipherText: String, expectedLetterFrequencies: Seq[Double], initialFrequencies: Map[Char, Int], shift: Int, language: Language): Double = {
@@ -58,7 +54,7 @@ object CaesarBreaker extends Breaker[Int]{
     }
   }
 
-  private def calculateExpectedLetterFrequencies(normalizedCipherText: String): Seq[Double] = {
+  private def calculateExpectedLetterFrequencies(normalizedCipherText: String, letterProbabilities: Seq[Double]): Seq[Double] = {
     val textLength = normalizedCipherText.length
     letterProbabilities.map(x => x * textLength)
   }
