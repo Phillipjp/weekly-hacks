72cb235c6f32003955928c2dba467087f357ba34
Phillip Perks <p.perks@elsevier.com>
2021-03-25 10:18:09 +0000

    [talking-clock] add tests for TalkingClock

--- talking-clock/src/test/scala/talkingclock/TalkingClockSpec.scala
+++ talking-clock/src/test/scala/talkingclock/TalkingClockSpec.scala
@@ -41,5 +41,37 @@ class TalkingClockSpec extends AnyFlatSpecLike with Matchers {
     actual shouldBe expected
   }
 
+  it should "give an am time when the hour is less than 12" in {
+    // Given, When, Then
+    TalkingClock.timeToSentence("02:00").get shouldBe "It's two am"
+  }
+
+  it should "give a pm time when the hour is greater than 11" in {
+    // Given, When, Then
+    TalkingClock.timeToSentence("12:00").get shouldBe "It's twelve pm"
+  }
+
+  it should "return None if the hour is invalid" in {
+    // Given, When, Then
+    TalkingClock.timeToSentence("24:00") shouldBe None
+  }
+
+  it should "return None if the minute is invalid" in {
+    // Given, When, Then
+    TalkingClock.timeToSentence("12:60") shouldBe None
+  }
+
+  it should "return None if the `:` is missing" in {
+    // Given, When, Then
+    TalkingClock.timeToSentence("1200") shouldBe None
+  }
+
+  it should "correctly return the time as a sentence" in {
+    // Given, When, Then
+    TalkingClock.timeToSentence("09:45").get shouldBe "It's nine forty five am"
+  }
+
+
+
 
 }
